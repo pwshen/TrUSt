175303e5b8ef4b2797d38385fc837147f5b9c236
benson <benson1016087@gmail.com>
2021-08-22 21:14:14 +0800

    Update download shell script

---
+++ download.sh
@@ -0,0 +1,14 @@
+# Data
+gdown "https://drive.google.com/uc?id=1G_0QANDZUmuqTFpkmKz_a9mH7RHk4wXp" -O ./data/
+# pretrained description encoder weight
+gdown "https://drive.google.com/uc?id=1kZNtNod6TtxyUO_-GgNFro6Yh0oOVFWW" -O ./data/
+gdown "https://drive.google.com/uc?id=1bMMl6nePkhPBidHwGDHKbM_V7fhywv7j" -O ./data/
+gdown "https://drive.google.com/uc?id=1_tjFFtx_heOEUFRGo9QRang1ZcCH2sWv" -O ./data/
+# Bert
+gdown "https://drive.google.com/uc?id=1qUEph2BtVSpHC-prqRtrvMU212g0qQyP" -O ./data/
+
+cd data
+unzip "*.zip" && rm *.zip
+
+
+
