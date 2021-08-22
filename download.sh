975cb82a6bae6d071b104043fea705e7ec09e1c6
pwshen <69194961+pwshen@users.noreply.github.com>
2021-08-23 00:09:48 +0800

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,9 +1,10 @@
 # Data
 gdown "https://drive.google.com/uc?id=1G_0QANDZUmuqTFpkmKz_a9mH7RHk4wXp" -O ./data/
-# pretrained description encoder weight
 gdown "https://drive.google.com/uc?id=1kZNtNod6TtxyUO_-GgNFro6Yh0oOVFWW" -O ./data/
 gdown "https://drive.google.com/uc?id=1bMMl6nePkhPBidHwGDHKbM_V7fhywv7j" -O ./data/
 gdown "https://drive.google.com/uc?id=1_tjFFtx_heOEUFRGo9QRang1ZcCH2sWv" -O ./data/
+# pretrained description encoder weight
+gdown "https://drive.google.com/uc?id=1sz2qqjOsehCSIz6u8lMBqHmRpdNW7TTk" -O ./data/
 # Bert
 gdown "https://drive.google.com/uc?id=1qUEph2BtVSpHC-prqRtrvMU212g0qQyP" -O ./data/
 
